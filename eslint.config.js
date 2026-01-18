510496c26c7c9eb77ce6303543a5f5b3007812d8
LeeJinyoung218 <amy021899@gmail.com>
2026-01-18 17:38:47 +0900

    chore: any 허용 설정

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+        // Unexpected any. Specify a different type 문구 제거
+        "@typescript-eslint/no-explicit-any": "off"
+    }
   },
 ])
