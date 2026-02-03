2ec3fb0d189753adc1d013793aca68a2adf012e9
LeeJinyoung218 <amy021899@gmail.com>
2026-02-03 21:28:52 +0900

    fix: 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import Layout from './Layout';
-import SignupPage from './pages/signup/SignupPage';
-import OAuthCallbackPage from './pages/signup/OAuthCallbackPage';
-import OAuthSignupPage from './pages/signup/OAuthSignupPage';
 import MainPage from './pages/MainPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -18,10 +15,35 @@ function App() {
           <Route element={<Layout />}>
             <Route path="/" element={<MainPage />} />
             {/* auth */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/oauth2/callback" element={<OAuthCallbackPage />} />
-            <Route path="/oauth2/signup" element={<OAuthSignupPage />} />
+            <Route path="/login" element={<LoginPage />}>
+              <Route path="mentee" element={<></>} />
+              <Route path="mentor" element={<></>} />
+            </Route>
+
+            {/* mentee */}
+            <Route path="/mentee" element={<></>}>
+              <Route path="planner" element={<></>}/>
+              <Route path="me" element={<></>} />
+              <Route path="feedback/:taskId" element={<></>} />
+            </Route>
+
+            {/* mentor */}
+            <Route path="/mentor" element={<></>}>
+              <Route path="mentee">
+                <Route index element={<></>} />
+                <Route path=":menteeId" element={<></>} />
+              </Route>
+              <Route path="feedback">
+                <Route index element={<></>} />
+                <Route path=":menteeId" element={<></>} />
+                <Route path=":menteeId/:taskId" element={<></>} />
+              </Route>
+              <Route path="task">
+                <Route index element={<></>} />
+                <Route path=":menteeId" element={<></>} />
+                <Route path=":menteeId/new" element={<></>} />
+              </Route>
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
