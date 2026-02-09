089b02f35976d1fba3605f64a10d079ffde4ba68
LeeJinyoung218 <amy021899@gmail.com>
2026-02-09 17:12:26 +0900

    feat: types 작성

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,5 @@
-export * from './task';
-export * from './apiResponse';
+export * as taskTypes from './task';
+export * as mentorTypes from './mentor';
+export * as imageTypes from './image';
+export * as menteeTypes from './mentee';
+// export * from './apiResponse';
